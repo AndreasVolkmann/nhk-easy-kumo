a17d6e9a7c59f6dc7431d26901ca5c6e260663fd
avo <avo@apurebase.com>
2018-01-21 18:22:40 +0100

    cleanup tokenizer

--- src/main/kotlin/me/avo/kumo/nhk/processing/ArticleTokenizer.kt
+++ src/main/kotlin/me/avo/kumo/nhk/processing/ArticleTokenizer.kt
@@ -2,23 +2,20 @@ package me.avo.kumo.nhk.processing
 
 import com.atilika.kuromoji.ipadic.*
 
-class ArticleTokenizer(val includeVerbs: Boolean) {
+class ArticleTokenizer(includeVerbs: Boolean) {
 
     private val tokenizer = Tokenizer()
 
-    fun tokenize(input: String): List<String> {
+    private val filters = listOf<(Token) -> Boolean>(
+            { it.partOfSpeechLevel1 == "名詞" || (includeVerbs && it.partOfSpeechLevel1 == "動詞") },
+            //{ it.partOfSpeechLevel1 !in listOf("接続詞", "記号") },
+            { it.partOfSpeechLevel2 == "一般" || it.partOfSpeechLevel2 == "自立" }
+    )
 
-        val filters = listOf<(Token) -> Boolean>(
-                { it.partOfSpeechLevel1 == "名詞" || (includeVerbs && it.partOfSpeechLevel1 == "動詞") },
-                //{ it.partOfSpeechLevel1 !in listOf("接続詞", "記号") },
-                { it.partOfSpeechLevel2 == "一般" || it.partOfSpeechLevel2 == "自立" }
-        )
-
-        return tokenizer
-                .tokenize(input)
-                .filter { token -> filters.all { it.invoke(token) } }
-                .map { it.baseForm }
-    }
+    fun tokenize(input: String): List<String> = tokenizer
+            .tokenize(input)
+            .filter { token -> filters.all { it.invoke(token) } }
+            .map { it.baseForm }
 
     fun analyzeWord(word: String) = tokenizer.tokenize(word).first().let {
         println(it)
